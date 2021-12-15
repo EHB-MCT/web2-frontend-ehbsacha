c2f065a33b211e720bede9ee7ffaa00088e97881
ehbsacha <71716345+ehbsacha@users.noreply.github.com>
2021-12-15 21:43:49 +0100

    Login working and comments
    Login is working and id stored

--- README.txt
+++ README.txt
@@ -6,3 +6,6 @@ remove "-" from the boardgame names: https://www.tutorialspoint.com/how-to-remov
 Used the string splitter from above with this link to select the gameid from the likebutton: https://stackoverflow.com/questions/651563/getting-the-last-element-of-a-split-string-array
 
 Login and signup dropdown: https://www.w3schools.com/css/css_dropdowns.asp
+A refreshment of localstorage: https://www.w3schools.com/jsref/prop_win_localstorage.asp
+
+Reload page on login/ signup: https://www.w3docs.com/snippets/javascript/how-to-reload-a-page-using-javascript.html
--- src/index.js
+++ src/index.js
@@ -1,50 +1,59 @@
 'use strict';
 
-const apiKey = "client_id=5UGynejyAW";
-var topGames = [];
-var popularGames = [];
+// variables
+const apiKey = "client_id=5UGynejyAW"; //apiKey
+var topGames = []; // Array for topGames
+var popularGames = []; // Array for popular games
 
 window.onload = async function(){
+  if(!localStorage.getItem("userId")){ // If userId doesn't exist
+    localStorage.setItem("userId", ""); // initialise the userId in localstorage
+  }
+  console.log(localStorage.getItem("userId"));
+
+  // Setup navbar (loggedin or not)
+  if(localStorage.getItem("userId") != ""){
+    document.getElementById("login").style.display = "none";
+    document.getElementById("loggedin").style.display = "flex";
+  }
+
   // fetch top games
   topGames = await fetchData(`https://api.boardgameatlas.com/api/search?order_by=rank&limit=12&ascending=false&${apiKey}`); // Fetch the top 8 games
-  buildList(topGames.games, 'topGames'); // Place the games on the page
+  buildList(topGames.games, 'topGames', "top"); // Place the games on the page
 
   // fetch popular games
   popularGames = await fetchData(`https://api.boardgameatlas.com/api/search?order_by=rank&limit=7&adescending=false&${apiKey}`); // Fetch the top 8 reddit mentioned games
-  buildList(popularGames.games, 'popularGames'); // Place the games on the page
+  buildList(popularGames.games, 'popularGames', "popular"); // Place the games on the page
 
   // Set correct images as background
   selectBackground(topGames.games[1], "bannerTopGames"); // Topgames background
   selectBackground(popularGames.games[2], "bannerPopularGames"); // Populargames backgound
 
   // After initialising open eventlistners
-  checkElements();
+  checkElements(); // activate eventListners
 };
 
-async function fetchData(someUrl){
-  return await fetch(someUrl)
-      .then(response => response.json());
+// A reusable fetch function
+async function fetchData(someUrl, method){
+  return await fetch(someUrl, method) // Return the value after fetch is done
+      .then(response => response.json()); // Make the returned readable
 }
 
-function buildList(games, htmlId){
-  // Order the list, now its ordered on the user rating
-  // games.sort(function(a,b) {
-  //     return b.average_user_rating - a.average_user_rating;
-  // });
-
+// Build the list of games to put in the html and make them visible
+function buildList(games, htmlId, partOfSite){
   //Change the innerHTML of the page
-  let html = '';
+  let html = ''; // Start clean
   //Make a for loop to pass all the games who are needed to be displayed
-  for(let game of games){
-      var newString = deString(game.handle, "-");
+  for(let game of games){ // Iterate over the sended array
+      var newString = deString(game.handle, "-"); // Do deString function. At funtion itself more information
       html += `
       <div class="game" value="${game.id}">
         <div class="name"><p>${newString}</p></div>
         <div class="data">
           <p class="rating">${game.rank}</p>
           <div class="buttons">
-            <button class="wishlist"><i class="fas fa-heart fa-2x" id="top_like_${game.id}"></i></button>
-            <button class="shelf"><i class="fas fa-bookmark fa-2x" id="top_shelf_${game.id}"></i></button>
+            <button class="wishlist"><i class="fas fa-heart fa-2x" id="${partOfSite}-like-${game.id}"></i></button>
+            <button class="shelf"><i class="fas fa-bookmark fa-2x" id="${partOfSite}-shelf-${game.id}"></i></button>
           </div>
           <img src="${game.image_url}" alt="Scythe">
           <div class="bottomBar">
@@ -54,12 +63,9 @@ function buildList(games, htmlId){
           </div>
         </div>
       </div>`;
-
-    // The calculation to round to the right numbers
-    // ${Math.round(game.average_learning_complexity * 100) / 100}
-
+      // The html for each game
   }
-  document.getElementById(htmlId).innerHTML = html;
+  document.getElementById(htmlId).innerHTML = html; // Put the builded games list into the right place in html with the corensponding id
 }
 
 // Change background img to the first boardgame of this place on the site
@@ -67,31 +73,32 @@ function selectBackground(game,id) {
   document.getElementById(id).style.backgroundImage = `url('${game.image_url}')`;
 }
 
-function checkElements() {
+// After initialising open eventlistners
+async function checkElements() {
   // Login and signup
   // If you click on login button in the navigation, show or hide the forms
   document.getElementById("login").addEventListener("click", function (event) {
     event.preventDefault();
     if(document.getElementById("filter").style.display == "flex"){
-      clearScreen();
+      clearScreen(); // If the login sceen is open close it
     }else{
-      showLogin();
+      showLogin(); // If the login screen in closed open it
     }
   });
+
   // if you click on the white background the login and signup forms close
   document.getElementById("filter").addEventListener("click", function (event) {
     event.preventDefault();
     clearScreen();
   });
 
+  // If you click on the loginsubmit do login action
   document.getElementById("loginSubmit").addEventListener("click", function (event) {
     event.preventDefault();
-    var name = document.getElementById("loginName").value;
-    var password = document.getElementById("loginPassword").value;
-    var json = {"userName": name, "userPassword": password};
-    console.log(name, password);
+    login();
   });
 
+  // Check for clicks in the chosen field, in this case for likes and shelf clicks
   document.getElementById('topGames').addEventListener('click', (event)=> {
     //Keep searching for the parent node to register the correct click
     const likeId = event.target.className.indexOf('game');
@@ -99,44 +106,60 @@ function checkElements() {
     // console.log(event.target.id);
   
     if(likeId){
+      // If the something which is clicked is has the word heart in the classname get the id and like or unlike the game
       if(event.target.className.indexOf('heart') !== -1){
-        var newid = selectId(event.target.id, "_");
+        var newid = selectId(event.target.id, "-"); // Get the id by removing the prefixes
         console.log('like', newid);
-
       }
       
+      // If the something which is clicked is has the word bookmark in the classname get the id and shelf or unshelf the game
       if(event.target.className.indexOf('bookmark') !== -1){
-        var newid = selectId(event.target.id, "_");
+        var newid = selectId(event.target.id, "-"); // Get the id by removing the prefixes
         console.log('bookmark', newid);
-
       }
-
     }
   });
 }
 
+// A function to remove the seperator and afterwards 
 function deString(string, separator){
-  //we split the string and make it free of separator
-  const separatedArray = string.split(separator);
-  //we join the separatedArray with empty string
-  const separatedString = separatedArray.join(" ");
-  return separatedString;
+  const separatedArray = string.split(separator);// We split the string and make it free of separator
+  const separatedString = separatedArray.join(" "); // We join the separatedArray with one space string
+  return separatedString; // Return the value
 }
 
+// the difference with the one above is that thisone selects the last part instead of joining them together
 function selectId(string, separator) {
-  //we split the string and make it free of separator
-  const separatedArray = string.split(separator);
-  //we join the separatedArray with empty string
-  const finalId = separatedArray[separatedArray.length - 1];
-  return finalId;
+  const separatedArray = string.split(separator); // We split the string and make it free of separator
+  const finalId = separatedArray[separatedArray.length - 1]; // Select the last part or the seperated array
+  return finalId; // Return the value
 }
 
-function showLogin() {
+// If loginbutton in the navbar gets clicked opens the login screen
+function showLogin() { // make parts of the site visibe
   document.getElementById("filter").style.display = "flex";
   document.getElementById("loginScreen").style.display = "flex";
 }
 
-function clearScreen(){
+function clearScreen(){ // Hides everything unnecessary
   document.getElementById("filter").style.display = "none";
   document.getElementById("loginScreen").style.display = "none";
+}
+
+// Login function gets called when clicking login and does a fetchcall to verify. If succes login else a error message
+async function login() {
+  try{ // Try to fetch
+    // I have skipped the varables for privacy reasons
+    var url = `https://web2-gameheaven-ehbsacha.herokuapp.com/user?name=${document.getElementById("loginName").value}&password=${document.getElementById("loginPassword").value}`;
+    const loggedIn = await fetchData(url,{method: 'GET'}); // Do the fetch and store return in loggedIn
+    console.log(loggedIn[0]);
+    saveId(loggedIn); // If already gotten to here login is succesfull and your id gets saved local
+  }catch(err){ 
+    document.getElementById("loginError").style.display = "flex"; // If you typed something wrong throw error
+  }
+}
+
+function saveId(userData){
+  localStorage.setItem("userId", userData[0]._id); // Save the userId so it counts as loggedIn
+  window.location.reload(true);
 }
